c6e428cdf12adcb1d9236f6980db06be7acd71c1
Mary-Faraj <maryfaraj85@gmail.com>
2023-12-14 11:35:57 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html', 'htm'] }));
+app.get('/hello', (req, res) => {
+    res.send('Hello World!')
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
